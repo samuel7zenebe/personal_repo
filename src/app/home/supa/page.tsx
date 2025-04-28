478e8a00dd5fbbb5955e256336bda3ec5a073dab
samuel <samuelzenebe7@gmail.com>
2025-04-28 14:15:41 +0300

    fixing-2

--- src/app/home/supa/page.tsx
+++ src/app/home/supa/page.tsx
@@ -1,49 +1,7 @@
-'use client'
-import { useEffect, useState } from 'react';
-import { supabase } from '@/utils/supabase/server';
-
-export default function Home() {
-  const [data, setData] = useState<{title:any,isbn:any}[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data: items, error } = await supabase.from('books').select("title,isbn");
-      if (error) console.error(error);
-      else setData(items);
-      setLoading(false);
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
-
-  return (
-    <div className=''>
-      <h1 className='mb-4 text-4xl bg-green-700 text-white p-4 text-center'>Data from Supabase</h1>
-      <table className='bg-slate-100 p-4'>
-        <thead className='bg-slate-200 font-bold'>
-        <tr >
-          <td className='p-2 m-1  border border-gray-300'>ID</td>
-          <td className='p-2 m-1  border border-gray-300'> Title </td>
-          <td className='p-2 m-1  border border-gray-300'> ISBN </td>
-          <td className='p-2 m-1  border border-gray-300'> Author </td>
-          <td className='p-2 m-1  border border-gray-300'> Genre </td>
-        </tr>
-        </thead>
-        <tbody>
-        {data.map(item => (
-        <tr key={item.isbn}>     
-          <td className='p-2 m-1  border border-gray-300'>{item.title}</td>
-          <td className='p-2 m-1  border border-gray-300'>{item.isbn}</td>     
-        </tr>
-        ))}
-      </tbody>
-      <caption className='caption-top text-sm text-thin font-light'>
-  Table 3.1 books presented by the FH Group.
-      </caption>
-      </table>
-    </div>
-  );
+function Page() {
+  return ( <h1 className="text-3xl">
+    Hello Supabase .....
+  </h1> );
 }
+
+export default Page;
