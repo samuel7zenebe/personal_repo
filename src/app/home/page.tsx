760885c0197d6dcd5af8f6ce9bf7f6cd773b6c3a
samuel <samuelzenebe7@gmail.com>
2025-04-29 11:24:18 +0300

    clean-ground

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,46 +1,9 @@
-"use client";
-import { useState, useEffect, useRef, ChangeEvent } from "react";
-
-export default function Menu() {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [pickedFiles, setPickedFiles] = useState<FileName[]>([]);
-
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      const file = e.target.files[0];
-      setPickedFiles([...pickedFiles, file.name]);
-    } else {
-      console.log("No image file chosen ");
-    }
-  };
+function Page() {
   return (
     <section>
-      <input
-        onChange={handleFileChange}
-        ref={inputRef}
-        hidden={true}
-        type="file"
-        name="file"
-        id="file"
-      />
-      <button
-        onClick={() => {
-          inputRef.current?.click();
-        }}
-        className="w-40 bg-green-800 text-white m-2 p-1 rounded-sm "
-      >
-        Select File
-      </button>
-      <div>
-        {pickedFiles.map((file) => (
-          <h2
-            key={file}
-            className="bg-slate-200 text-2xl text-green-800 p-1 m-1"
-          >
-            {file}
-          </h2>
-        ))}
-      </div>
+      <h1 className="mb-4 text-green-600 bg-slate-50">Home Page </h1>
     </section>
   );
 }
+
+export default Page;
