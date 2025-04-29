a8371e7749b72f4e4108aaa4d372b99db8dfd5b1
samuel <samuelzenebe7@gmail.com>
2025-04-29 11:16:32 +0300

    Revert "fix-end"
    This reverts commit 86f75a99e7a013e9efaaa4c83d2445191e098c6c.

--- src/app/home/supa/page.tsx
+++ src/app/home/supa/page.tsx
@@ -1,44 +1,7 @@
-'use client'
-import { useState, useEffect } from "react";
-
-interface Resource {
-  id: number,
-  userId: number,
-  title: string,
-  body: string
-}
-
 function Page() {
-  const [resource, setResource] = useState<Resource[]>([])
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-        if (!response.ok) {
-          throw new Error("Request failed.");
-        }
-        const data = await response.json();
-        setResource(data);
-      } catch {
-        console.log("error...occured..")
-      }
-
-    }
-    fetchData();
-  })
-
-  return (<>
-    {
-      resource.filter(x => x.id < 4).map((res) => (
-        <div key={res.id}>
-          <h1 >{res.title}</h1>
-          <h1 className="text-red-900 text-3xl">{res.id}</h1>
-        </div>))
-
-    }    <div>
-            <h1> Hello man </h1>
-    </div>
-  </>);
+  return ( <h1 className="text-3xl">
+    Hello Supabase .....
+  </h1> );
 }
 
 export default Page;
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -3,7 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const         dsfd = "jdfds";
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
