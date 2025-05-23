7b9213fba2928aa5b42c5865a01941c759a3339d
samuel <samuelzenebe7@gmail.com>
2025-05-23 13:48:01 +0300

    strict export type

--- src/app/recommended/[itemId]/page.tsx
+++ src/app/recommended/[itemId]/page.tsx
@@ -12,27 +12,29 @@ const ItemPage: FC<PageProps> = async ({ params }) => {
   const feedBacks = await feedBack();
 
   return (
-    <section className="px-4">
-      <div className="grid grid-cols-4 grid-rows-1 font-extrabold text-lg/relaxed">
-        <h1>ID</h1>
-        <h1>Name</h1>
-        <h1>Email</h1>
-        <h1>Content</h1>
-      </div>
-      <div>
-        {feedBacks?.map((user) => (
-          <div
-            className="grid grid-cols-4 grid-rows-1 bg-slate-200"
-            key={user.id * Math.floor(Math.random() * 100)}
-          >
-            <h1>{user.id}</h1>
-            <h1>{user.name}</h1>
-            <h1>{user.email}</h1>
-            <h1>{user.content}</h1>
-          </div>
-        ))}
-      </div>
-    </section>
+    <>
+      <section className="px-4">
+        <div className="grid grid-cols-4 grid-rows-1 font-extrabold text-lg/relaxed">
+          <h1>ID</h1>
+          <h1>Name</h1>
+          <h1>Email</h1>
+          <h1>Content</h1>
+        </div>
+        <div>
+          {feedBacks?.map((user) => (
+            <div
+              className="grid grid-cols-4 grid-rows-1 bg-slate-200"
+              key={user.id * Math.floor(Math.random() * 100)}
+            >
+              <h1>{user.id}</h1>
+              <h1>{user.name}</h1>
+              <h1>{user.email}</h1>
+              <h1>{user.content}</h1>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
